8764724c73069ca2c77017116ede1cd90b8f4ffc
ForTombstone <nefothingy@hotmail.com>
2021-03-12 02:29:48 +0000

    Ok

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Nefomemes Network accounts
+
+Nefomemes 
--- src/signup.js
+++ src/signup.js
@@ -1,36 +1,44 @@
+
+
 var passwordInput = $('#password-input')[0];
 var passwordErrorBox = $('#password-error')[0];
 var emailInput = $('#email-input')[0];
 var emailErrorBox = $('#email-error')[0];
 var usernameInput = $('#username-input')[0];
 var usernameErrorBox = $("#username-error")[0];
-
+var captchaErrorBox = $("#captcha-error")[0];
+var agreeErrorBox("#agree-error")[0];
+var captchaDone = false;
 function validateUsername(settings){
 	var isError = false;
 	
 	if(usernameInput.value){
 	try {
-		
+		if(settings.isSubmit){
   ( async () => {
 	let { data } =	await axios.get(`/api/userExist?user=${usernameInput.value}`);
 	
-	if(data.exist) throw data;
+	if(data.data.userExisted) throw data;
   })();
+		} 
 	} catch(data){
 		isError = true;
 		usernameErrorBox.innerHTML = "A user already exist with that name.";
 		
 	}
- 	} else {
+ 	} else if(settings.isSubmit) {
+ 	
  		isError = true;
  		usernameErrorBox.innerHTML = "Please insert a username you want to use";
+ 	} else {
+ 		
  	};
  	
  	
  	if(isError){
 usernameErrorBox.style.display = 'unset';
  	} else {
- usernameErrorBox.style.color = 'red';
+ usernameErrorBox.style.display = 'none';
  	}
  	
 }
@@ -59,17 +67,27 @@ function validatePassword(settings = {}) {
 		}
 	}
 
-	if (isWrong && (empty ? settings.isSubmit : true)) {
+	if (isWrong && (empty ? (settings.isSubmit)  : true)) {
 		passwordErrorBox.style.display = 'unset';
 		passwordErrorBox.style.color = 'red';
 		return false;
+		
 	} else if (passwordRate.length >= 8) {
 		passwordErrorBox.innerHTML = `Your password strength: ${
 			passwordRate.value
 		}`;
+
 		passwordErrorBox.style.color = 'black';
+		
+		if(passwordRate.id === 0){
+			passwordErrorBox.style.color = 'red';
+		return false;
+		}
+		
+		
 		return true;
 	} else {
+		
 		passwordErrorBox.style.display = 'none';
 		return true;
 	}
@@ -82,15 +100,11 @@ function regexEmail(email) {
 }
 function validateEmail(settings = {}) {
 	if (
-		regexEmail(emailInput.value) ||
-		(settings.isSubmit && settings.isSubmit === true
-			? false
-			: !emailInput.value)
+		regexEmail(emailInput.value) || ((!emailInput.value ? (!settings.isSubmit) : false) )
 	) {
 		emailErrorBox.style.display = 'none';
 		return true;
 	} else {
-		console.log(emailInput.value.length);
 		emailErrorBox.innerHTML = 'Please insert a valid email address';
 		emailErrorBox.style.display = 'unset';
 
@@ -98,19 +112,68 @@ function validateEmail(settings = {}) {
 	}
 }
 
+function validateCaptcha(){
+	
+	if(hcaptcha.getResponse()){
+		
+		captchaErrorBox.style.display = 'none';
+		return true;
+	} else {
+		captchaErrorBox.innerHTML = 'Please complete the Captcha';
+		captchaErrorBox.style.display = 'unset';
+		return false;
+	}
+	
+}
+
+
+
+
+function validateAgreement(){
+	
+	if()
+	
+}
+
+
+
 function onFormSubmit(e) {
-var emailAllowed = validateEmail({isSubmit: true});
-	var emailAllowed = validateEmail({ isSubmit: true });
-	var pwdAllowed = validatePassword({ isSubmit: true });
-	var allowed = emailAllowed && pwdAllowed;
+var usernameAllowed = validateUsername({isSubmit: true});
+var	 emailAllowed = validateEmail({ isSubmit: true });
+var	passwordAllowed = validatePassword({ isSubmit: true });
+var captchaAllowed = validateCaptcha();
+
+var allowrd = usernameAllowed && emailAllowed && passwordAllowed && captchaAllowed;
+
+
+	
+	
 	if (!allowed) {
 		return e.preventDefault();
 	}
 }
+
+function captchaCallback(){
+
+captchaDone = true;
+
+validateCaptcha();
+
+	
+}
+
+
+
+
 $(document).ready(() => {
 	$('#signup-box form')[0].addEventListener('submit', onFormSubmit);
 
 	passwordInput.addEventListener('input', validatePassword);
+	
+	usernameInput.addEventListener('input', validateUsername);
 
 	emailInput.addEventListener('input', validateEmail);
 });
+
+const log = document.querySelector('.event-log-contents');
+
